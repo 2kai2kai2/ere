623e8fd81904d7e55dc5e363e81747ff6eb78da1
2kai2kai2 <22033723+2kai2kai2@users.noreply.github.com>
2025-10-29 16:00:29 -0700

    Expand benchmarks
    - Add comparison with `exec`
    - Potential for multi-engine comparison
    - Add rgb value benchmark (we actually win on that one)

--- benches/ere_benchmark.rs
+++ benches/ere_benchmark.rs
@@ -4,6 +4,88 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use ere::{compile_regex, Regex};
 use pprof::criterion::{Output, PProfProfiler};
 
+macro_rules! key_to_engine {
+    ($re:literal, pikevm) => {
+        ("pikevm", ::ere::compile_regex_pikevm!($re))
+    };
+    ($re:literal, pikevm_u8) => {
+        ("pikevm_u8", ::ere::compile_regex_u8pikevm!($re))
+    };
+    ($re:literal, one_pass_u8) => {
+        ("one_pass_u8", ::ere::compile_regex_u8onepass!($re))
+    };
+    ($re:literal, fixed_offset) => {
+        ("fixed_offset", ::ere::compile_regex_fixed_offset!($re))
+    };
+}
+
+/// ## Usage:
+/// `compile_engines!("some regex", engine[, engines...])`
+///
+/// Where valid engine ids are:
+/// - `pikevm` for [`::ere::compile_regex_pikevm`]
+/// - `pikevm_u8` for [`::ere::compile_regex_u8pikevm`]
+/// - `one_pass_u8` for [`::ere::compile_regex_u8onepass`]
+/// - `fixed_offset` for [`::ere::compile_regex_fixed_offset`]
+///
+/// ## Output
+/// Pairs with labels and regexes e.g.
+/// `("pikevm", <regex struct>), ...`
+macro_rules! compile_engines {
+    ($re:literal, $($engines:ident),+$(,)?) => {
+        [
+            $(
+                key_to_engine!($re, $engines)
+            ),+
+        ]
+    }
+}
+
+fn rgb_simple(c: &mut Criterion) {
+    // test all relevant engines
+    const REGEXES: [(&'static str, Regex<4>); 1] = compile_engines!(
+        r"^#([[:alnum:]]{2})([[:alnum:]]{2})([[:alnum:]]{2})$",
+        // pikevm,
+        // pikevm_u8,
+        // one_pass_u8,
+        fixed_offset,
+    );
+
+    let regex_runtime =
+        ::regex::Regex::new(r"^#([[:alnum:]]{2})([[:alnum:]]{2})([[:alnum:]]{2})$").unwrap();
+
+    let mut group = c.benchmark_group("rgb_simple");
+    for i in 0..5 {
+        let mut hasher = ::std::hash::DefaultHasher::new();
+        hasher.write_u8(i);
+        let [r, g, b, ..] = hasher.finish().to_le_bytes();
+        let haystack = format!("#{r:02x}{g:02x}{b:02x}");
+
+        for (engine_name, engine) in REGEXES.iter() {
+            group.bench_with_input(
+                BenchmarkId::new(format!("ere {engine_name} (test)"), &haystack),
+                &haystack,
+                |b, s| b.iter(|| assert!(engine.test(s))),
+            );
+            group.bench_with_input(
+                BenchmarkId::new(format!("ere {engine_name} (exec)"), &haystack),
+                &haystack,
+                |b, s| b.iter(|| assert!(engine.exec(s).is_some())),
+            );
+        }
+        group.bench_with_input(
+            BenchmarkId::new("regex (test)", &haystack),
+            &haystack,
+            |b, s| b.iter(|| assert!(regex_runtime.is_match(s))),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("regex (exec)", &haystack),
+            &haystack,
+            |b, s| b.iter(|| assert!(regex_runtime.captures(s).is_some())),
+        );
+    }
+}
+
 fn ipv4(c: &mut Criterion) {
     const REGEX: Regex<5> = compile_regex!(
         r"^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])$"
@@ -24,9 +106,19 @@ fn ipv4(c: &mut Criterion) {
             |b, s| b.iter(|| assert!(REGEX.test(s))),
         );
         group.bench_with_input(
+            BenchmarkId::new("ere (exec)", &haystack),
+            &haystack,
+            |b, s| b.iter(|| assert!(REGEX.exec(s).is_some())),
+        );
+        group.bench_with_input(
             BenchmarkId::new("regex (test)", &haystack),
             &haystack,
             |b, s| b.iter(|| assert!(regex_runtime.is_match(s))),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("regex (exec)", &haystack),
+            &haystack,
+            |b, s| b.iter(|| assert!(regex_runtime.captures(s).is_some())),
         );
     }
 }
@@ -62,9 +154,19 @@ fn big_haystack_1(c: &mut Criterion) {
             |b, s| b.iter(|| assert!(REGEX.test(s))),
         );
         group.bench_with_input(
+            BenchmarkId::new("ere (exec)", i),
+            haystack.as_str(),
+            |b, s| b.iter(|| assert!(REGEX.exec(s).is_some())),
+        );
+        group.bench_with_input(
             BenchmarkId::new("regex (test)", i),
             haystack.as_str(),
             |b, s| b.iter(|| assert!(regex_runtime.is_match(s))),
+        );
+        group.bench_with_input(
+            BenchmarkId::new("regex (exec)", i),
+            haystack.as_str(),
+            |b, s| b.iter(|| assert!(regex_runtime.captures(s).is_some())),
         );
     }
     group.finish();
@@ -73,6 +175,6 @@ fn big_haystack_1(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-    targets = ipv4, big_haystack_1
+    targets = rgb_simple, ipv4, big_haystack_1
 }
 criterion_main!(benches);
